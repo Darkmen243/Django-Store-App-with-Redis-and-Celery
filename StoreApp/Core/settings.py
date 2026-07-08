773f5358774c01592c4bd66d38e62a48c01f241e
Darkmen243 <evseenko.vowa@yandex.ru>
2026-07-08 23:10:15 +0300

    fixed app definitions in settings file

--- StoreApp/Core/settings.py
+++ StoreApp/Core/settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Users',
+    'StoreApp.Users',
     'rest_framework',
     'rest_framework_simplejwt',
-    'Products',
-    'Orders',
+    'StoreApp.Products',
+    'StoreApp.Orders',
 ]
 
 MIDDLEWARE = [
